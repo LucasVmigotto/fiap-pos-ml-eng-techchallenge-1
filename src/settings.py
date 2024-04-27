bedc75765d87488c8daea61ff2e2a42a096062ab
Lucas Vidor Migotto <lvmigotto1@gmail.com>
2024-04-27 15:53:46 -0300

    feat(env): add data origin url and data structure
    Add type in settings props

--- src/settings.py
+++ src/settings.py
@@ -1,13 +1,37 @@
 from os import getenv
+from pathlib import Path
 
 
 class Settings:
 
-    DATA_FOLDER_PATH = getenv('API_DATA_FOLDER_PATH',
-                         '/app/data/raw/')
+    DATA_ORIGIN_URL = getenv('API_DATA_ORIGIN_URL',
+                             'http://vitibrasil.cnpuv.embrapa.br/download/')
 
-    JWT_SECRET = getenv('API_JWT_SECRET',
-                        'jwtsecret')
+    DATA_RAW_STRUCTURE: dict[str, str] = dict(
+        producao=dict(producao='Producao.csv'),
+        processamento=dict(viniferas='ProcessaViniferas.csv',
+                           americanas='ProcessaAmericanas.csv',
+                           mesa='ProcessaMesa.csv',
+                           sem_classificacao='ProcessaSemclass.csv'),
+        comercializacao=dict(comercio='Comercio.csv'),
+        importacao=dict(mesa='ImpVinhos.csv',
+                        espumantes='ImpEspumantes.csv',
+                        frescas='ImpFrescas.csv',
+                        passas='ImpPassas.csv',
+                        suco='ImpSuco.csv'),
+        exportacao=dict(mesa='ExpVinho.csv',
+                        espumantes='ExpEspumantes.csv',
+                        frescas='ExpUva.csv',
+                        suco='ExpSuco.csv')
+    )
+
+    __DATA_FOLDER_PATH_VALUE = getenv('API_DATA_FOLDER_PATH',
+                                      '/app/data/raw/')
+
+    DATA_FOLDER_PATH: Path = Path(__DATA_FOLDER_PATH_VALUE)
+
+    JWT_SECRET: str = getenv('API_JWT_SECRET',
+                             'jwtsecret')
 
 
 settings = Settings()
