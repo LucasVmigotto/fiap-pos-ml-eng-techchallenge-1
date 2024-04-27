c4f03e067ad887b5e43f561a04b1e14c0e74f623
Lucas Vidor Migotto <lvmigotto1@gmail.com>
2024-04-27 17:32:39 -0300

    feat(api): create data endpoint
    Return data from a dataframe
    Opt filter by rows and columns
    Opt return format - json (default) or csv

--- src/routers/data.py
+++ src/routers/data.py
@@ -1,4 +1,7 @@
-from fastapi import APIRouter
+from typing import Annotated
+from fastapi import APIRouter, HTTPException, Query
+
+from utils import read_dataframe_from_code
 
 
 router = APIRouter(
@@ -7,6 +10,30 @@ router = APIRouter(
 )
 
 
-@router.get('/{category}/{data_file}')
-def get_data(category: str, data_file: str):
-    return {category: data_file}
+@router.get(
+    path='/{dataframe_code}',
+    description='Return the selected rows and columns of a chosen by code dataframe'
+)
+def get_data(dataframe_code: str,
+             row_start: int = None,
+             row_end: int = None,
+             columns: Annotated[list[str] | None, Query()] = None,
+             as_json: bool = True):
+    try:
+        df = read_dataframe_from_code(dataframe_code)
+
+        rows_selected = range(row_start, row_end) if row_start is not None and row_end is not None else df.index
+
+        return_df = df.loc[rows_selected, columns or df.columns]
+
+        return return_df.to_json() if as_json else return_df.to_csv(index=False)
+
+    except ValueError as err:
+        raise HTTPException(409, str(err))
+
+    except FileNotFoundError as err:
+        raise HTTPException(404, str(err))
+
+    except Exception as err:
+        print(err)
+        raise HTTPException(500, 'An unexpected error occurred')
