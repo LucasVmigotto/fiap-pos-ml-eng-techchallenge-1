caf60270dee06172020bb1c712a060a46f78ee1b
Lucas Vidor Migotto <lvmigotto1@gmail.com>
2024-04-27 17:08:15 -0300

    refactor(utils): create read_dataframe_from_code function
    Use read_dataframe_from_code in info endpoint

--- src/routers/info.py
+++ src/routers/info.py
@@ -1,11 +1,10 @@
 from os import listdir
-from os.path import exists
 from pathlib import Path
 
-import pandas as pd
 from fastapi import APIRouter, HTTPException
 
 from settings import settings
+from utils import read_dataframe_from_code
 
 
 router = APIRouter(
@@ -21,9 +20,11 @@ router = APIRouter(
 def list_dataframes():
     try:
         data_files = [f'{category}.{data_file[:-4]}'
-                    for category in listdir(settings.DATA_FOLDER_PATH)
-                    for data_file in listdir(Path(settings.DATA_FOLDER_PATH) / category)]
+                      for category in listdir(settings.DATA_FOLDER_PATH)
+                      for data_file in listdir(Path(settings.DATA_FOLDER_PATH) / category)]
+
         return {'dataframes': data_files}
+
     except Exception as err:
         print(err)
         return HTTPException(500, 'An unexpected error occurred')
@@ -35,23 +36,18 @@ def list_dataframes():
 )
 def info_dataframe(dataframe_cod: str):
     try:
-        if len(dataframe_cod.split('.')) != 2:
-            return HTTPException(409, 'Malformed dataframe code')
-
-        category, dataframe = dataframe_cod.split('.')
-
-        DF_PATH = Path(settings.DATA_FOLDER_PATH) / category / f'{dataframe}.csv'
-
-        if not exists(DF_PATH):
-            return HTTPException(404, detail='Dataframe not found')
-
-        df = pd.read_csv(DF_PATH,
-                        sep=';' if category != 'processamento' else '\t').iloc[:, 1:]
+        df = read_dataframe_from_code(dataframe_cod)
 
         return {'shape': {'rows': df.shape[0],
-                        'columns': df.shape[1]},
+                          'columns': df.shape[1]},
                 'columns': {key: value.name for key, value in df.dtypes.to_dict().items()}}
+
+    except ValueError as err:
+        raise HTTPException(409, str(err))
+
+    except FileNotFoundError as err:
+        raise HTTPException(404, str(err))
 
     except Exception as err:
         print(err)
-        return HTTPException(500, 'An unexpected error occurred')
+        raise HTTPException(500, 'An unexpected error occurred')
