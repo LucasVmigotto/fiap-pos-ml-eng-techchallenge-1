c7afadea4872ddcea4b8526a9e251d1c8487d9b6
Lucas Vidor Migotto <lvmigotto1@gmail.com>
2024-05-18 12:31:22 +0000

    fix(auth): use credentials pydantic model to authenticate
    Remove default username and password verification

--- src/routers/auth.py
+++ src/routers/auth.py
@@ -1,34 +1,28 @@
 import logging
 from logging import Logger
-from typing import Annotated
 
-from fastapi import APIRouter, Depends, HTTPException, status
-from fastapi.security import OAuth2PasswordRequestForm
+from fastapi import APIRouter
 
-from schemas.auth import Token
+from schemas.auth import Token, Credentials
 from security.auth import encode
-from settings import settings
 
 logger: Logger = logging.getLogger(__name__)
 router = APIRouter(
     prefix='/auth',
     tags=['Auth']
 )
-DEFAULT_USERNAME: str = settings.get_config('api', 'auth', 'default_username')
-DEFAULT_PASSWORD: str = settings.get_config('api', 'auth', 'default_password')
 
 
 @router.post(
     path='/token',
     description='Return, given correct auth credentials, a access token'
 )
-def auth_user(credentials: Annotated[OAuth2PasswordRequestForm, Depends()]) -> Token:
+def auth_user(credentials: Credentials) -> Token:
     try:
-        if credentials.username != DEFAULT_USERNAME or credentials.password != DEFAULT_PASSWORD:
-            raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
-                                detail='Username or Password incorrect')
-
-        return Token(access_token=encode({'sub': credentials.username}))
+        return Token(access_token=encode({
+            'sub': credentials.username,
+            'email': credentials.email
+        }))
 
     except Exception as err:
         logger.exception(err, exc_info=True)
